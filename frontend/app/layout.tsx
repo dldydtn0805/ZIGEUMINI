e76e4d9c1d3d138b331b49fe7b7a29e3a3376ff3
dldydtn0805 <139421083+dldydtn0805@users.noreply.github.com>
2026-07-07 17:37:05 +0900

    Refactor custom.d.ts

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 import WebSocket from "./WebSocket";
 import QueryProvider from "./QueryProvider";
 const inter = Inter({ subsets: ["latin"] });
--- frontend/custom.d.ts
+++ frontend/custom.d.ts
@@ -14,3 +14,10 @@ declare module "*.wav" {
   const src: string;
   export default src;
 }
+
+declare module "*.css";
+
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
